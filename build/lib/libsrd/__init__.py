4e711170c907923a78f49da7ab85d9a89dc47a87
Sam Davis <srd076@icloud.com>
2024-10-30 18:41:54 +0000

    Add GitHub Link

--- build/lib/libsrd/__init__.py
+++ build/lib/libsrd/__init__.py
@@ -1,5 +1,17 @@
 """
-# LibSrd
+# LibSrd 
+
+## Modules
+image_convert.py  
+merge_pdf.py  
+table.py  
+
+## Commands
+1. ```mergepdfs```  
+Will merge all pdf's found in the current directory, and save the result at: ./Output/MergePdfOutput.pdf  
+  
+2. ```imgconvert {InitalFormat} {FinalFormat}```  
+Will convert all images of InitalFormat in current directory to FinalFormat in ./Output/
 """
 
 from libsrd.image_convert import convert_images
